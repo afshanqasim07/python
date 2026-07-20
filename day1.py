35558dd7ba8f1ba87ecb6e3e6a078465fee26eec
afshanqasim07 <afshaanqasim07@gmail.com>
2026-07-20 17:02:49 +0500

    datatypes

---
+++ day1.py
@@ -0,0 +1,12 @@
+mobile = {
+    "productname": "iphone",
+    "version": 12,
+    "price": 120000.0000,
+    "is_true": True,
+    "phonelist": ["samsung", "motorolla", "vivo"],
+    "phonepricelist": (75000, 55000, 25000),
+    "phoneidnumber": {3, 5, 9},
+    "result": None
+}
+
+print(mobile)
