d8241b90e0e81e4e70bd72214ae73715c1cff96d
afshanqasim07 <afshaanqasim07@gmail.com>
2026-07-25 01:44:46 +0500

    string methods

--- Strings.py
+++ Strings.py
@@ -5,15 +5,40 @@
 # print(type(x))
 
 # STRINGS
-Firstname = "Afshan"
-sentence = "shelikesapple"
-print(Firstname,sentence)
-# METHODS OF STRINGS
-# INDEXING
-print(Firstname[3])
-# SLICING
-print(sentence[3:8])
-print(sentence[-8:-3])
-print(sentence[3:])
-print(sentence[:8])
-# strings operation
+Firstname ="Afshan"
+Lastname ="Qasim"
+sentence = "she Likes Apple"
+# print(Firstname,sentence)
+# # METHODS OF STRINGS
+# # INDEXING
+# print(Firstname[3])
+# # SLICING
+# print(sentence[3:8])
+# print(sentence[-8:-3])
+# print(sentence[3:])
+# print(sentence[:8])
+# # strings operation
+# # 1.concationation
+# print(Firstname+Lastname)
+# # 2.Repitation
+# print(Firstname * 3)
+# some random methods
+# print(Firstname.upper())
+# print(Firstname.lower())
+# print(Firstname.capitalize())
+# print(sentence.title())
+# print(Firstname.strip())
+# print(sentence.replace("apple", "mango"))
+# print(sentence.find("d"))
+# print(sentence.count("a"))
+# print(Firstname.startswith("A"))
+# print(Firstname.startswith("S"))
+# print(Lastname.endswith("m"))
+# print(Lastname.endswith("a"))
+# print(sentence.split())
+# print(sentence.split())
+fruits = ["Apple", "Banana", "Mango"]
+print(",".join(fruits))     
+print(Firstname.isalpha())
+print(Firstname.isnumeric()) 
+print(Firstname.isalnum())
