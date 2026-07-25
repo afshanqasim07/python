7a610c6c91b415d7e10e73c0092fc4c7ef00a310
afshanqasim07 <afshaanqasim07@gmail.com>
2026-07-25 18:27:56 +0500

    operatos

---
+++ operators.py
@@ -0,0 +1,23 @@
+# operators
+a = 10
+b = 15
+# airthematic operators
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# print(a**b)
+# print(a%b)
+
+# comparision
+# print(a==b)
+# print(a!=b)
+# print(a>=b)
+# print(a<=b)
+# print(a>b)
+# print(a<b)
+# logical
+print(a>b and b<a)
+print(a>b or a<b)
+print(not(a>b or b<a))
+
