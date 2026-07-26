d4a035c844bb2c092cee0a32f0b3611b64b0cfe5
afshanqasim07 <afshaanqasim07@gmail.com>
2026-07-26 15:19:14 +0500

    if else

---
+++ conditional.py
@@ -0,0 +1,50 @@
+# conditional statments
+# num = int(input("Enter The Number :"))
+# print(num)
+# if(num%2 == 0):
+#     print("its a even number")
+# else:
+#     print("its a odd number")
+
+# age = int(input("Enter your age :" ))
+# print(age)
+# if(age >= 18):
+#     print("Eligible to vote")
+# else:
+#     print("Not Eligible to vote")
+
+# num = int(input("Enter The Number :"))
+# print(num)
+# if(num> 0):
+#     print("its a positive number")
+# elif(num<0):
+#     print("its a negative number")
+# else:
+#     print("its a zero")
+
+# a = int(input("Enter The Number :"))
+# print(a)
+# b = int(input("Enter The Number :"))
+# print(b)
+# c= int(input("Enter The Number :"))
+# print(c)
+# if(a>b and a>c):
+#     print("a is greater")
+# elif(b>a and b>c):
+#     print("b is greater then a")
+# else:
+#     print("c is greater then a and b")
+
+marks = int(input("Enter The marks :"))
+print(marks)
+if(marks>=90):
+    print("you got A+")
+elif(marks>=80):
+    print("you got A")
+elif(marks >=70 ):
+    print("you got B")
+elif(marks>=60):
+    print("you got C")
+else :
+    print("FAILED! BETTER LUCK NEX TIME")
+
