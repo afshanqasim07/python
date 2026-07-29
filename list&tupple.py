b2db66057ad7350b7fcaf203e16ea0bc3d34d5a9
afshanqasim07 <afshaanqasim07@gmail.com>
2026-07-29 19:30:12 +0500

    sets

--- list&tupple.py
+++ list&tupple.py
@@ -1,35 +1,45 @@
-# list
-fruits  = ["apple", "banana", "cherry","orange","mango"]
-numbers = [2,3,5,8,7,9]
-mixed = [10, "Ali", True, 5.5]
-# list operations
-# indexing
-print(fruits)
-print(fruits[0])
-# change item
-fruits[2]= "lichi"
-print(fruits)
-fruits.append("cherry")
-print(fruits)
-fruits.insert(4,"grapes")
-print(fruits)
-fruits.remove("lichi")
-print(fruits)
-fruits.pop(4)
-print(fruits)
-fruits.clear()
-print(fruits)
+# # list
+# fruits  = ["apple", "banana", "cherry","orange","mango"]
+# numbers = [2,3,5,8,7,9]
+# mixed = [10, "Ali", True, 5.5]
+# # list operations
+# # indexing
+# print(fruits)
+# print(fruits[0])
+# # change item
+# fruits[2]= "lichi"
+# print(fruits)
+# fruits.append("cherry")
+# print(fruits)
+# fruits.insert(4,"grapes")
+# print(fruits)
+# fruits.remove("lichi")
+# print(fruits)
+# fruits.pop(4)
+# print(fruits)
+# fruits.clear()
+# print(fruits)
+# print(len(fruits))
+# print("apple" in fruits)
+# print("lichi" not in fruits)
+# # # slicing
+# # print(fruits[1:5])
+# numbers.sort()
+# print(numbers )
+# numbers.sort(reverse = True)
+# print(numbers )
+
+
+# # tupples
+# print(fruits[3])
+
+
+# set
+fruits  = {"apple", "banana", "cherry","orange","mango", "banana"}
 print(len(fruits))
+print(fruits)
 print("apple" in fruits)
-print("lichi" not in fruits)
-# # slicing
-# print(fruits[1:5])
-numbers.sort()
-print(numbers )
-numbers.sort(reverse = True)
-print(numbers )
-
-
-# tupples
-fruits  = ("apple", "banana", "cherry","orange","mango")
-print(fruits[3])
+fruits.add("lichi")
+print(fruits)
+fruits.remove("banana")
+print(fruits)
