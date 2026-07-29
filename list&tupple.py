8fab8cd9a15a5ea466608cd9d852cba8300efc5c
afshanqasim07 <afshaanqasim07@gmail.com>
2026-07-29 18:58:29 +0500

    tuples

--- list&tupple.py
+++ list&tupple.py
@@ -4,27 +4,32 @@ numbers = [2,3,5,8,7,9]
 mixed = [10, "Ali", True, 5.5]
 # list operations
 # indexing
-# print(fruits)
-# print(fruits[0])
-# # change item
-# fruits[2]= "lichi"
-# print(fruits)
-# fruits.append("cherry")
-# print(fruits)
-# fruits.insert(4,"grapes")
-# print(fruits)
-# fruits.remove("lichi")
-# print(fruits)
-# fruits.pop(4)
-# print(fruits)
-# fruits.clear()
-# print(fruits)
-# print(len(fruits))
-# print("apple" in fruits)
-# print("lichi" not in fruits)
+print(fruits)
+print(fruits[0])
+# change item
+fruits[2]= "lichi"
+print(fruits)
+fruits.append("cherry")
+print(fruits)
+fruits.insert(4,"grapes")
+print(fruits)
+fruits.remove("lichi")
+print(fruits)
+fruits.pop(4)
+print(fruits)
+fruits.clear()
+print(fruits)
+print(len(fruits))
+print("apple" in fruits)
+print("lichi" not in fruits)
 # # slicing
 # print(fruits[1:5])
 numbers.sort()
 print(numbers )
 numbers.sort(reverse = True)
 print(numbers )
+
+
+# tupples
+fruits  = ("apple", "banana", "cherry","orange","mango")
+print(fruits[3])
