4c23bc9a381905b826c6328c48f3495696edb9f9
afshanqasim07 <afshaanqasim07@gmail.com>
2026-07-31 14:19:28 +0500

    loops

---
+++ loops.py
@@ -0,0 +1,14 @@
+fruits  = ["apple", "banana", "cherry","orange","mango"]
+numbers = [2,3,5,8,7,9]
+# for x in fruits:
+#     print(x)
+#     if x == "banana":
+#         break
+
+# for i in range(2,22,2):
+#     print(i)
+# while loop
+i = 1
+while(i<= 6):
+    print(i)
+    i+=2
