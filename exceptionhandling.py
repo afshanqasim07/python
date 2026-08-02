75075d511a0db93e0c52983cbda9538e3a6e6a1b
afshanqasim07 <afshaanqasim07@gmail.com>
2026-08-02 20:52:44 +0500

    error handling

---
+++ exceptionhandling.py
@@ -0,0 +1,10 @@
+# try:
+#     age = int(input("Enter your age:"))
+#     print(age)
+# except ValueError:
+#     print("enter the age number only")
+try:
+     with open ("my_note.txt" ,"r") as file:
+        print("FILE EXIST")
+except FileNotFoundError:
+    print("NO DATASET EXIST")
